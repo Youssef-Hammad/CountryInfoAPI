626838bdecd41960ffd8024109878b5581a885c8
Ali El-Masry <alimasryy48@gmail.com>
2020-03-18 22:09:11 +0200

    added testcase

--- tests/unit/CountryInfoHandlerTest.py
+++ tests/unit/CountryInfoHandlerTest.py
@@ -17,5 +17,15 @@ class TestCountryInfoHandler(unittest.TestCase):
         result = Self.countryInfoHandler.get('egypt')
         Self.assertEqual({'name' : 'Egypt'}, result)
 
+    def test_get_no_info(Self):
+        Self.countryInfoHandler = CountryInfoHandler()
+        Self.countryInfoHandler.parser = ParserStub({})
+        Self.countryInfoHandler.countryInfo = CountryInfoStub({
+            'name' : 'Egypt', 
+            'region' : 'Africa'
+            })
+        result = Self.countryInfoHandler.get('egypt')
+        Self.assertEqual({'name' : 'Egypt', 'region' : 'Africa'}, result)
+
 if __name__ == '__main__':
     unittest.main()
