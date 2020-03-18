bdf2faffe074870b4f9952e5a396f617e952fc81
Ali El-Masry <alimasryy48@gmail.com>
2020-03-18 21:05:45 +0200

    modified CountryInfoHandler for easier unit testing and added a test case

---
+++ tests/unit/CountryInfoHandlerTest.py
@@ -0,0 +1,21 @@
+import sys
+sys.path.append('../..')
+
+import unittest
+from ParserStub import ParserStub
+from CountryInfoStub import CountryInfoStub
+from CountryInfoHandler import CountryInfoHandler
+
+class TestCountryInfoHandler(unittest.TestCase):
+    def test_get_field_exist(Self):
+        Self.countryInfoHandler = CountryInfoHandler()
+        Self.countryInfoHandler.parser = ParserStub({'info' : 'name'})
+        Self.countryInfoHandler.countryInfo = CountryInfoStub({
+            'name' : 'Egypt', 
+            'region' : 'Africa'
+            })
+        result = Self.countryInfoHandler.get('egypt')
+        Self.assertEqual({'name' : 'Egypt'}, result)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/unit/ParserStub.py
@@ -0,0 +1,5 @@
+class ParserStub:
+    def __init__(Self, args):
+        Self.args = args
+    def parse_args(Self):
+        return Self.args
