097de336dfa1e1f79c647949450eb091258e7984
Ali El-Masry <alimasryy48@gmail.com>
2020-03-18 22:37:08 +0200

    fixed tests

--- CountryInfo.py
+++ CountryInfo.py
@@ -4,4 +4,6 @@ class CountryInfo:
     def get(Self, name):
         url = 'https://restcountries.eu/rest/v2/name/' + name
         req = requests.get(url)
+        if req.status_code == 404:
+            return {}
         return req.json()[0];
