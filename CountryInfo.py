02755a5dc9aa5fcb148a9e7847649edb554a7b3b
Ali El-Masry <alimasryy48@gmail.com>
2020-04-23 14:09:37 +0200

    cached the responses to decrease the use of the external API

--- CountryInfo.py
+++ CountryInfo.py
@@ -1,9 +1,14 @@
 import requests
 
+countries = {}
+
 class CountryInfo:
     def get(Self, name):
+        if name in countries:
+            return countries[name]
         url = 'https://restcountries.eu/rest/v2/name/' + name
         req = requests.get(url)
         if req.status_code == 404:
             return {}
-        return req.json()[0];
+        countries[name] = req.json()[0]
+        return req.json()[0]
